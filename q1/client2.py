e00357346dd4efbc4c43b8b63ecbbc47b449266d
vijayganesh-sde <93527877+vijayganesh-sde@users.noreply.github.com>
2023-08-13 23:27:01 +0530

    Delete q1 directory

--- q1/client2.py
+++
@@ -1,6 +0,0 @@
-import socket
-c2Socket=socket.socket()
-c2Socket.connect(('localhost',9090))
-datafromServer=c2Socket.recv(1024).decode()
-print("Data From server",datafromServer)
-c2Socket.close()
